edf1cec75ff8d4fc9cafb09a141d192aeca03daf
asmojo2327067-cloud <as.mojo2327067@gmail.com>
2026-08-30 18:21:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ma-pwa-v44';
+const CACHE_NAME = 'ma-pwa-v45';
 const CORE = [
   './',
   './index.html',
@@ -19,37 +19,30 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      ))
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
       .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   if(event.request.method !== 'GET') return;
-
   if(event.request.mode === 'navigate'){
     event.respondWith(
       fetch(event.request)
         .then(response => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
+          const copy=response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put('./index.html',copy));
           return response;
         })
         .catch(() => caches.match('./index.html'))
     );
     return;
   }
-
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      if(cached) return cached;
-      return fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      });
-    })
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy=response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request,copy));
+      return response;
+    }))
   );
 });
