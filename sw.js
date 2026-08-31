73074d9aedd35e914f99c7278bca146021966f4d
asmojo2327067-cloud <as.mojo2327067@gmail.com>
2026-08-31 23:48:52 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,52 +1,25 @@
-const CACHE_NAME = 'ma-pwa-v60';
-const CORE = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png',
-  './apple-touch-icon.png'
-];
+const CACHE_NAME = 'ma-pwa-v72';
+const CORE = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(CORE))
-      .then(() => self.skipWaiting())
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      ))
-      .then(() => self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', event => {
   if(event.request.method !== 'GET') return;
-
   if(event.request.mode === 'navigate'){
-    event.respondWith(
-      fetch(event.request)
-        .then(response => {
-          const copy=response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put('./index.html',copy));
-          return response;
-        })
-        .catch(() => caches.match('./index.html'))
-    );
+    event.respondWith(fetch(event.request).then(response => {
+      const copy=response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put('./index.html',copy));
+      return response;
+    }).catch(() => caches.match('./index.html')));
     return;
   }
-
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-      const copy=response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request,copy));
-      return response;
-    }))
-  );
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy=response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request,copy));
+    return response;
+  })));
 });
