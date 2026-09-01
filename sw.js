2766b1a0510463643c7f7b263ea75e523383b861
asmojo2327067-cloud <as.mojo2327067@gmail.com>
2026-09-01 10:09:19 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ma-pwa-v72';
+const CACHE_NAME = 'ma-pwa-v73';
 const CORE = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install', event => {
